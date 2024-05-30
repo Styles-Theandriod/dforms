4e92ad970dd6012590e230f465d950febb111f4e
Styles-Theandriod <emmanuelajah188@gmail.com>
2024-05-30 14:40:16 +0100

    i change something from urls.py inside accounts.urls

--- dforms/accounts/urls.py
+++ dforms/accounts/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from .views import accounts, register, login, profile, main
-
+from .views import accounts, register, login, profile
 
 app_name = 'accounts'
 urlpatterns = [
-    path('', main, name='accounts'),
     path('register/', register, name='register'),
     path('login/', login, name='login'),
     path('profile/', profile, name='profile')
